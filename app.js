110ec902c43f04494e295200da96b00bc8c382c7
Wess <brownw15@gmail.com>
2019-12-02 02:01:01 -0800

    CRD Working Update not functional

--- app.js
+++ app.js
@@ -29,22 +29,45 @@ app.get('/', function (req, res) {
     res.write("</form>");
     res.write("</div>");
     res.write("<div>");
+
+    res.write("<form action='/users/' method='post'>");
+    res.write("<h2> Choose user</h2>");
+    res.write("<input type='text' name= 'acc_email' placeholder='Enter your email address used to sign up'/>");
+    res.write("<button type='submit' class='btn btn-primary'>Confirm</button>");
+    res.write("</form>");
+
+    res.write("<div>");
     res.write("<form action='/updateUser' method='post'>");
     res.write("<h2> Update User info</h2>");
     res.write("<input type='text' name= 'acc_id' placeholder='user ID'/>");
-    res.write("<input type='text' name='FirstName' placeholder='First'/>");
+    res.write("<input type='text' name='FirstName' placeholder='First/>");
     res.write("<input type='text' name='LastName' placeholder='Last'/>");
     res.write("<button type='submit' class='btn btn-primary'>Save Update</button>")
     res.write("</form>");
     res.write("</div>");
+
     res.write("<div>");
     res.write("<form action='/deleteUser' method='post'>");
     res.write("<h2>Delete a User </h2>");
-    res.write("<input type='hidden' name='acc_id' required>")
+    res.write("<input type='hidden' name='acc_id' required>");
     res.write("<input type='text' name= 'email_address' placeholder='user email'/>");
-    res.write("<button type='submit' class='btn btn-primary'>Save Delete</button>")
+    res.write("<button type='submit' class='btn btn-primary'>Save Delete</button>");
     res.write("</form>");
     res.write("</div>");
+
+    res.write("<div>");
+    res.write("<form action='/showValues' method='post'>");
+    res.write("<select>");
+    res.write("<option value= 1 name='option1'>First Name</option>");
+    res.write("<option value= 2 name='option2'>Last Name</option>");
+    res.write("<option value=3 name='option3'>Email</option>");
+    res.write("</select>");
+    res.write("<button type='submit' class='btn btn-primary'>Find</button>");
+    res.write("</form>");
+    res.write("</div>");
+
+
+    res.write("<a href='/users'><button> show users</button</a>")
     res.write("</body>");
     res.write("</html>");
     res.end();
@@ -55,28 +78,50 @@ dbConn.connect();
  app.get('/')
 // Retrieve all users 
 app.get('/users', function (req, res) {
+    
     dbConn.query('SELECT * FROM USER_ACCOUNT', function (error, results, fields) {
         if (error) throw error;
         return res.send({ error: false, data: results, message: 'users list.' });
     });
 });
+
+//Show user info
+app.get('/showValues', function (req, res) {
+    let showValues = req.body.option1;
+    console.log(showValues);
+
+    if(showValues== 1){
+    dbConn.query('SELECT  FirstName FROM USER_ACCOUNT', function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+        res.send({ error: false, data: results, message: 'users list.' });
+    });
+    }
+    else if (showValues== 2)
+        dbConn.query('SELECT  LastName FROM USER_ACCOUNT', function (error, results, fields) {
+            if (error) throw error;
+            console.log(results);
+            res.send({ error: false, data: results, message: 'users list.' });
+        });
+        else (showValues==3)
+        dbConn.query('SELECT  Email FROM USER_ACCOUNT', function (error, results, fields) {
+            if (error) throw error;
+            console.log(results);
+            return res.send({ error: false, data: results, message: 'users list.' });
+        });
+});
  
- 
-// Retrieve user with id 
-/*app.get('/user/:id', function (req, res) {
-  
-    let id = req.params.id;
-    console.log(id);
-    if (!id) {
+//Retrieve user with id 
+app.get('/users/:acc_email',(req, res)=> {
+    let acc_email = req.params.acc_email;
+    if (!acc_email) {
         return res.status(400).send({ error: true, message: 'Please provide user_id' });
     }
-  
-    dbConn.query('SELECT * FROM USER_ACCOUNT where acc_id=?', id, function (error, results, fields) {
+    dbConn.query('SELECT * FROM USER_ACCOUNT where acc_id = ?', acc_email, function (error, results, fields) {
         if (error) throw error;
-        return res.send({ error: false, data: results[0], message: 'no user associated list.' });
+        return res.send(result);
     });
-  
-}); */
+}); 
  
  
 // Add a new user  
@@ -91,11 +136,11 @@ app.post('/saveUser', (req, res)=> {
  
  
 //  Update user with id
-app.post('/updateUser', (req, res) => {
+app.put('/updateUser/:acc_id', (req, res) => {
     let newF= req.body.firstName;
     let newL= req.body.lastName;
-    let user_id = req.body.acc_id;
-    let sql = 'UPDATE USER_ACCOUNT SET FirstName =' + "dbConn.escape(newF)"+ ', LastName ='+ 'dbConn.escape(newL)' + 'WHERE acc_id =' + dbConn.escape(user_id);
+    let user_id = req.params.acc_id;
+    let sql = 'UPDATE USER_ACCOUNT SET FirstName =' + 'dbConn.escape(newF)'+ ', LastName ='+ 'dbConn.escape(newL)' + 'WHERE acc_id =' + dbConn.escape(user_id);
     let query = dbConn.query(sql, (err, results) => {
         if(err) throw err;
         res.redirect('/');
@@ -111,10 +156,15 @@ app.post('/deleteUser', function (req, res) {
         res.redirect('/');
     });
   });
+ 
+
 
 // set port
 app.listen(3000, function () {
     console.log('Node app is running on port 3000');
 });
+
+
+
  
 module.exports = app;
